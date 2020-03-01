8deff84732f9774c45996efe1e1ce7e714f33cc8
EC2 Default User <ec2-user@ip-172-31-21-142.us-east-2.compute.internal>
2020-03-01 01:56:28 +0000

    dog app

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
-import click
 
+import click
 @click.command()
 def hello():
-    click.echo('Hello World!')
+    click.echo('Hello World, this is Maggie!')
 
+
+
+def dog():
+    dog = ['Chester', 'Coco Chanel']
+    for i, name in enumerate(dog):
+         print ("My dog's name is {name}!".format(name=name))
+         
 if __name__ == '__main__':
-    hello()
+    dog()
